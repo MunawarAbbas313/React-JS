52fed14a05f4b199700170b7e6319d6b345e2dbc
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-03-13 06:17:54 +0500

    Add files via upload
    Read the Read me

---
+++ DAY1/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+// Creating a Functional Componet :
+// Name should be Starting with a Capital word 
+
+
+// In default React App only the app component is Renderd 
+function App() { // Here inside the funciton we are writing JS and HTML known as JSX
+  
+// Look how a varible id Directly Integrated into HTML
+let greet = " HELLO REACT "
+return(
+   <h1>{greet}</h1>
+);
+  
+}
+
+export default App
