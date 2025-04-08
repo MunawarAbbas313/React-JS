606b1e9e8eef9d1a6d6dad8afbfc88c6c897ccff
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-04-09 00:55:08 +0500

    Create signup.jsx

---
+++ Full stack Ecommerce using firebase/signup.jsx
@@ -0,0 +1,15 @@
+
+import "../pages/ButtonsStyle.css";
+import Login from "./Login";
+function Signup(){
+   
+    const SignUp = "SignUp"
+
+    return (
+        <>
+       
+        <Login SetHead={SignUp}/>
+        </>
+    )
+}
+export default Signup;
