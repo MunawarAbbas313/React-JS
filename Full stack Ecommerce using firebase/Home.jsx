1cbbaafb5c5e5657003c8f92d927182cb1e3183b
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-04-10 00:10:17 +0500

    Create Home.jsx

---
+++ Full stack Ecommerce using firebase/Home.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import Products from "./Products";
+import Cart from "./Cart";
+
+function Home(){
+    const [products , setProducts] = useState([]);
+    const [cart , setCrat] = useState([]);
+    const [itemsIncCt , setItemsIncCt] = useState(0);
+    const [showCart , setShowCart] = useState(false);
+    const handleBackToProducts = ()=>{
+        setShowCart(false);
+        setItemsIncCt(cart.length);
+        setCrat([]);
+        
+    }
+
+
+
+    const HandleCart = (product)=>{
+        setCrat(prev => [...prev, product]);
+        setItemsIncCt(prev => prev + 1);
+
+    }
+    
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
+    const HandleGotoCart = ()=>{
+        setShowCart(true);
+    }
+    
+    return (
+        <div className="home">
+            <h2 className="hPg">Welcome to the Home Page</h2>
+            <div className="cartPgLogo">
+                
+            </div>
+            {showCart ? (
+                <div className="cartPage">
+                    
+                    <Cart cartItems={cart} setGoBack={handleBackToProducts}/>
+                </div>
+            ) : (
+                <Products products={products}  addToCart={HandleCart} ToatlItemsInCart= {itemsIncCt} HandleGotoCart={HandleGotoCart}/>
+
+            )
+            }
+            
+
+        </div>
+    );
+}
+
+export default Home;
